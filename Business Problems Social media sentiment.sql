69d6903ca47cac79443846351fe3de9c966a6c6d
NILADRI DAS <167008575+nild-ds@users.noreply.github.com>
2024-12-30 21:37:52 +0530

    Add files via upload

---
+++ Business Problems Social media sentiment.sql
@@ -0,0 +1,16 @@
+--1.List the countries and number of posts where the sentiment score is negative.
+--2.Identify the platform with the highest total engagement.
+--3.List the platforms along with their total engagement.
+--4.Identify and list the users by the count of there contents.
+--5.Identify the hour of the day with the highest average sentiment score.
+--6.List hashtags that appear more than one time in posts with a positive sentiment.
+--7.Find the most frequently used hashtag for each sentiment.
+--8.Calculate the rolling average of engagement (retweets + likes) for each country based on the timestamp.
+--9.Rank sentiments based on their average engagement for each platform.
+--10.Find the hour with the highest number of positive sentiment posts.
+--11.Calculate the total engagement (retweets + likes) for each month and identify trends.
+--12.List all posts with engagement (retweets + likes) above the platform average.
+--13.Calculate the average engagement (retweets + likes) per sentiment for each country.
+--14.Examine changes in sentiment over time for each platform to detect shifts in user mood or content reception.
+--15.Compare the monthly engagement and sentiment across different platforms to identify which platform performs best in various regions.
+
